290d4809babbbef97c7e2b272e82aa6712c204bc
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2023-06-12 17:16:58 +0000

    Create beautifularray.py

---
+++ onboarding/beautifularray.py
@@ -0,0 +1,26 @@
+
+n = int(input())
+arr = list(map(int, input().split()))
+ 
+set1 = []
+set2 = []
+set3 = []
+ 
+for num in arr:
+    if num < 0:
+        set1.append(num)
+    elif num > 0:
+        set2.append(num)
+    else:
+        set3.append(num)
+ 
+if len(set2) == 0:
+    set2.append(set1.pop())
+    set2.append(set1.pop())
+ 
+if len(set1) % 2 == 0:
+    set3.append(set1.pop())
+ 
+print(len(set1), *set1)
+print(len(set2), *set2)
+print(len(set3), *set3)
