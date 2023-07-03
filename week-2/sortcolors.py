ffbc52bf015a826086dd45eceeb313ef6eff4368
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2023-07-03 22:19:38 +0000

    Create sortcolors.py

---
+++ week-2/sortcolors.py
@@ -0,0 +1,19 @@
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            sorted_nums = True
+            for j in range(len(nums) - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    sorted_nums = False
+            
+            if sorted_nums:
+                 break
+
+"""
+Time: O(N^2)
+Space: O(1)
+"""
