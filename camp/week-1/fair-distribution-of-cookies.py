858eeaa07d9514985da7e60ad55b4a7655f28f17
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2023-11-10 20:39:27 +0000

    Added solution for Fair Distribution of Cookies

---
+++ camp/week-1/fair-distribution-of-cookies.py
@@ -0,0 +1,21 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        state = [0]*k
+        ans = sum(cookies)
+
+        def search(state, idx):
+            nonlocal ans
+            if idx >= len(cookies):
+                r = min(state)
+                if r == 0: return
+                ans = min(ans, max(state))
+                return
+
+            for i in range(k):
+                state[i] += cookies[idx]
+                if state[i] < ans:
+                    search(state, idx+1)
+                state[i] -= cookies[idx]
+
+        search(state, 0)
+        return ans
