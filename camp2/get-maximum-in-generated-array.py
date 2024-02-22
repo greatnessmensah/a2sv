735a52e6afa339e644ea761496dba9ed3d1ca28d
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2024-02-22 16:07:43 +0000

    Update get-maximum-in-generated-array.py

--- camp2/get-maximum-in-generated-array.py
+++ camp2/get-maximum-in-generated-array.py
@@ -1,22 +1,35 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        def dp(i):
-            if i < 2:
-                memo[i] = i
-                return i
+#         def dp(i):
+#             if i < 2:
+#                 memo[i] = i
+#                 return i
             
-            if memo[i] == -1:
-                if i % 2:
-                    memo[i] = dp((i-1) // 2) + dp((i-1) // 2 + 1)
-                else:
-                    memo[i] = dp(i // 2)
+#             if memo[i] == -1:
+#                 if i % 2:
+#                     memo[i] = dp((i-1) // 2) + dp((i-1) // 2 + 1)
+#                 else:
+#                     memo[i] = dp(i // 2)
 
-            return memo[i]
+#             return memo[i]
         
-        memo = [-1] * (n+1)
+#         memo = [-1] * (n+1)
         
-        for i in range(n, -1, -1):
-            dp(i)
+#         for i in range(n, -1, -1):
+#             dp(i)
             
-        return max(memo)
+#         return max(memo)
+        if n < 2:
+            return n
+        
+        dp = [0 for _ in range(n+1)]
+        dp[1] = 1
+        
+        for i in range(2, n+1):
+            if i % 2:
+                dp[i] = dp[(i-1) // 2] + dp[(i-1) // 2+1]
+            else:
+                dp[i] = dp[i // 2]
+                
+        return max(dp)
         
