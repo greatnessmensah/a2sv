9bff965924e4a0089defc081e9a1ebe1274f4ec5
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2024-02-22 15:44:31 +0000

    Update house-robber.py

--- camp2/house-robber.py
+++ camp2/house-robber.py
@@ -1,14 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dp(i):
-            if i >= len(nums):
-                return 0
+#         def dp(i):
+#             if i >= len(nums):
+#                 return 0
             
-            if i not in memo:
-                memo[i] = max(dp(i+2) + nums[i], dp(i+1))
+#             if i not in memo:
+#                 memo[i] = max(dp(i+2) + nums[i], dp(i+1))
             
-            return memo[i]
+#             return memo[i]
         
-        memo = {}
-        return dp(0)
+#         memo = {}
+#         return dp(0)
     
+        dp = [0, 0]
+        
+        for i in range(len(nums)):
+            temp = max(dp[-1]+nums[i], dp[-2])
+            dp[-1] = dp[-2]
+            dp[-2] = temp
+            
+        return dp[-2]
