e68221874e0bd0c7853a22d4fd03c64399b1b8b5
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2024-02-22 15:41:07 +0000

    Update coin-change.py

--- camp2/coin-change.py
+++ camp2/coin-change.py
@@ -1,25 +1,38 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def dp(i):
-            if i == 0:
-                return 0
-            if i < 0:
-                return float("inf")
+#         def dp(i):
+#             if i == 0:
+#                 return 0
+#             if i < 0:
+#                 return float("inf")
             
-            mini = float("inf")
+#             mini = float("inf")
                     
-            if i not in memo:
-                for coin in coins:
-                    mini = min(mini, 1 + dp(i - coin))
-                memo[i] = mini
+#             if i not in memo:
+#                 for coin in coins:
+#                     mini = min(mini, 1 + dp(i - coin))
+#                 memo[i] = mini
                 
-            return memo[i]
+#             return memo[i]
         
-        memo = defaultdict(int)
-        res = dp(amount)
+#         memo = defaultdict(int)
+#         res = dp(amount)
         
-        return res if res != float("inf") else -1
+#         return res if res != float("inf") else -1
+
+        if amount == 0:
+            return 0
+    
+        dp = [float("inf") for _ in range(amount+1)]
         
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            mini = float("inf")
+            for coin in coins:
+                if i - coin >= 0:
+                    mini = min(mini, 1 + dp[i-coin])
+            dp[i] = mini
                 
-            
-                
+        return dp[amount] if dp[amount] != float("inf") else -1
+        
