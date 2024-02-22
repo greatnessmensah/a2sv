96dfbce59f650634274cd9fc396bbce7cac80d77
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2024-02-22 15:51:38 +0000

    Update n-th-tribonacci-number.py

--- camp2/n-th-tribonacci-number.py
+++ camp2/n-th-tribonacci-number.py
@@ -1,17 +1,31 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        def dp(i):
-            if i == 0:
-                return 0
-            if i < 3:
-                return 1
+#         def dp(i):
+#             if i == 0:
+#                 return 0
+#             if i < 3:
+#                 return 1
             
-            if memo[i] == -1:
-                memo[i] = dp(i-1) + dp(i-2) + dp(i-3)
+#             if memo[i] == -1:
+#                 memo[i] = dp(i-1) + dp(i-2) + dp(i-3)
                 
-            return memo[i]
+#             return memo[i]
             
-        memo = [-1] * (37+1)
+#         memo = [-1] * (37+1)
         
-        return dp(n)
+#         return dp(n)
+
+        if n == 0:
+            return n
+        elif n < 3:
+            return 1
+        
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 0
+        dp[1] = dp[2] = 1
+        
+        for i in range(3, len(dp)):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+            
+        return dp[n]
         
