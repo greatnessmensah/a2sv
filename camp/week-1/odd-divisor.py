f6476cd317724a0be06fd10d7658d12a7fe154de
Ephraim Mensah <92410699+greatnessmensah@users.noreply.github.com>
2024-03-25 15:53:20 +0000

    Create odd-divisor.py

---
+++ camp/week-1/odd-divisor.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    a = int(input())
+    
+    while a % 2 == 0:
+        a = a // 2
+    if a != 1:
+        print('YES') 
+    else :
+        print('NO')
