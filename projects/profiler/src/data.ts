607909d83a992984abe14d0a71ad769cc61d3a65
greatnessmensah <ephraimmensah99@gmail.com>
2024-04-07 21:30:48 +0000

    added images

--- projects/profiler/src/data.ts
+++ projects/profiler/src/data.ts
@@ -1,11 +1,10 @@
-// profiles.ts
-
 export interface UserProfile {
     id: number;
     name: string;
     bio: string;
     deparment?: string;
     website?: string;
+    imageUrl?: string;
   }
   
   export const userProfiles: UserProfile[] = [
@@ -15,6 +14,7 @@ export interface UserProfile {
       deparment: 'Engineering',
       bio: 'Software Engineer',
       website: 'https://johndoe.com',
+      imageUrl: './public/avatar1.png',
     },
     {
       id: 2,
@@ -22,16 +22,19 @@ export interface UserProfile {
       deparment: 'Research',
       bio: 'Physicist',
       website: 'https://janesmith.com',
+      imageUrl: './public/avatar2.png',
     },
     {
       id: 3,
       name: 'Bob Johnson',
       deparment: 'Marine Biology',
       bio: 'Marine Scientist',
+      imageUrl: './public/avatar3.png',
     },
     {
       id: 4,
       name: 'Alice Brown',
+      deparment: 'Engineering',
       bio: 'Electrical Engineer',
       website: 'https://alicebrown.com',
     }
