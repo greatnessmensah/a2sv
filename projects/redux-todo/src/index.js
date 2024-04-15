3f59cf5922d8a8222d8628261d1f3bbad66bebad
greatnessmensah <ephraimmensah99@gmail.com>
2024-04-15 16:59:38 +0000

    added edit task action

--- projects/redux-todo/src/index.js
+++ projects/redux-todo/src/index.js
@@ -2,6 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import store from './store'
+
+// console.log('Initial state: ', store.getState())
+
+// const unsubscribe = store.subscribe(() =>
+//   console.log('State after dispatch: ', store.getState())
+// )
+
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+
+// store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+// store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+// store.dispatch({
+//   type: 'filters/colorFilterChanged',
+//   payload: { color: 'red', changeType: 'added' },
+// })
+
+// unsubscribe()
+
+// console.log('Dispatching action')
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+// console.log('Dispatch complete')
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
 
 
 ReactDOM.render(
